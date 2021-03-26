379d8034f25371c9ab4529b2362883a570fc2f16
fabio98p <parisifabio98@gmail.com>
2021-03-26 16:24:12 +0100

    boolzapp fine layout html css

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+	console.log('ciao ale');
+
+	var root = new Vue(
+		{
+			el: '#root',
+			data: {
+
+			},
+			methods: {
+				
+			},
+			computed: {
+
+			},
+		}
+	);
+});
